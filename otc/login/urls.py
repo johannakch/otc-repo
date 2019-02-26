871abe0a04d06a24cfef617ee21b93e8f9bb3d09
johannakch <jojo.ko@icloud.com>
2019-02-26 09:53:24 +0100

    added 'change password' function

--- otc/login/urls.py
+++ otc/login/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'', include('django.contrib.auth.urls')),
+    url('login/', views.login, name='login'),
+    url('logout/', views.logout, name='logout'),
+    url('^password-change/done/$', views.password_change_done,
+        {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
+    url('^password-change/$', views.password_change,
+        {'template_name': 'registration/password_change_form.html', 'post_change_redirect': 'password_change_done'}, name='password_change'),
+
+
 ]
