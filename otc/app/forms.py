e7403198521d92bb36ae85e86601044432a94596
Viviane Rehor <viviane.rehor@web.de>
2019-03-16 17:13:24 +0100

    pop up und formular um zeiten dropdown erweitert

--- otc/app/forms.py
+++ otc/app/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import CheckboxSelectMultiple
-from django.forms import Select
-
+import datetime
+HOUR_CHOICES = [(datetime.time(hour=x), '{:02d}:00'.format(x)) for x in range(8, 24)]
 from app.models import Event
 from app.models import GameTypeChoice
 
@@ -12,8 +12,15 @@ class EventForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         is_basic_user = kwargs.pop('is_basic_user')
+        y = kwargs.pop('year')
+        m = kwargs.pop('month')
+        d = kwargs.pop('day')
         super(EventForm, self).__init__(*args, **kwargs)
-        print("Basic" + str(is_basic_user))
+        events = Event.objects.filter(day=datetime.date(year=int(y), month=int(m), day=int(d)))
+        eventTimes = [int(x.start_time.strftime("%H")) for x in events]
+        times = [x for x in range(9,24) if x not in eventTimes]
+        print(times[1])
+        self.fields['start_time'].widget.choices = [(datetime.time(hour=x), '{:02d}:00'.format(x)) for x in range(9, 24) if x not in eventTimes]
         if is_basic_user:
             self.fields['type'].choices = [(tag.value, tag.value) for tag in GameTypeChoice if tag.value == "Einzelspiel" or tag.value == "Doppelspiel"]
         else:
@@ -22,3 +29,4 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ('type', 'start_time', 'players', 'duration', 'externPlayer1', 'externPlayer2', 'externPlayer3', 'notes')
+        widgets = {'start_time': forms.Select(choices=HOUR_CHOICES)}
--- otc/app/views.py
+++ otc/app/views.py
@@ -11,6 +11,7 @@ from app.forms import EventForm
 from app.utils import EventCalendar, get_year_dic, hasReservationRight
 from app.models import Event
 
+from django.contrib import messages
 
 def index(request):
     #print("REQUEST:",request.GET)
@@ -30,7 +31,8 @@ def add_event(request, year, month, day):
     context['date'] = format_date(day, month, year)
     iba = (not (request.user.is_staff) and not (request.user.is_superuser) and request.user.is_active)
     if request.method == 'POST':
-        new_event_form = EventForm(request.POST, is_basic_user=iba)
+        #new_event_form = EventForm(request.POST, is_basic_user=iba)
+        new_event_form = EventForm(request.POST, is_basic_user=iba, year=year, month=month, day=day)
         if new_event_form.is_valid():
             new_event = new_event_form.save(commit=False)
             new_event.creator = request.user
@@ -46,11 +48,12 @@ def add_event(request, year, month, day):
         if (hasReservationRight(request.user, int(year), int(month), int(day))):
             # boolean der form und html verändert, je nachdem ob es ein basic user oder ein staff/superuser ist
             context['is_basic_user'] = iba
-            context['form'] = EventForm(is_basic_user=iba)
+            #context['form'] = EventForm(is_basic_user=iba)
+            context['form'] = EventForm(is_basic_user=iba, year=year, month=month, day=day)
         else :
             print("Error: No Reservationright")
+            messages.info(request, 'Du hast in dieser Woche kein Recht mehr weitere Reservierungen vorzunehme.')
             return HttpResponseRedirect(reverse('index'))
-
     print(context['form'])
 
     return render(request, 'app/add_event.html', context)
@@ -67,7 +70,8 @@ def show_event(request, id):
     # wenn aktueller user creator oder einer der players ist -> bearbeitbares form anzeigen
     if (event.creator == request.user or len(Event.objects.filter(players__id=request.user.id))>0):
         context['is_basic_user'] = iba
-        context['form'] = EventForm(is_basic_user=iba, instance=event)
+        #context['form'] = EventForm(is_basic_user=iba, instance=event)
+        context['form'] = EventForm(is_basic_user=iba, instance=event, year=event.year, month=event.month, day=event.day)
         return render(request, 'app/add_event.html', context)
     else:
         players_list = [player.get_full_name() for player in event.players.all() if event.players.all()]
