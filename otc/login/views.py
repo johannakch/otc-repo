e0b24c6245245380732ad58893700f1741df6a97
johannakch <jojo.ko@icloud.com>
2019-03-16 15:45:06 +0100

    request new passwort + email sending process complete + created own google account

--- otc/login/views.py
+++ otc/login/views.py
@@ -1,28 +1,38 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 
 def request_new_password(request):
-    context = {}
+    context = {'sendmail_success': False}
     if request.method == 'POST':
-        print(request.POST)
         if 'request_new_pw' in request.POST:
             superusers = [user.email for user in User.objects.filter(is_superuser=True)]
-            print(superusers)
             subject = 'Reservierungssystem OTC - Anfrage zur Passwortänderung'
             name = request.POST['username']
-            message = '{} benötigt ein neues Passwort. Bitte neues Passwort zuweisen.'.format(name)
-            email = request.POST['email']
-            from_email = email
-            to_email = superusers
+            user_email = request.POST['email']
+            message = '{} ({}) benötigt ein neues Passwort. Bitte neues Passwort zuweisen.'.format(name, user_email)
             try:
-                send_mail(subject, message, from_email, [to_email[0]])
+                email = EmailMessage(subject, message, to=superusers)
+                code = email.send()
+                if code == 1:
+                    context.update({
+                        'sendmail_success': True
+                    })
+                else:
+                    context.update({
+                        'sendmail_success': False,
+                        'tried_to_send': True,
+                    })
             except Exception as e:
-                print(e)
-                print("failed")
+                #print(e)
+                context.update({
+                    'sendmail_success': False,
+                    'tried_to_send': True,
+                    #'admin_contact': 'mariusschmitt@algrande.net'
+                })
 
 
 
