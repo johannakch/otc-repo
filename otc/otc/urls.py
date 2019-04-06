ed303bf5de7429bde0a321c6394b54df519204aa
johannakch <jojo.ko@icloud.com>
2019-04-06 19:42:32 +0200

    Hütchen raus

--- otc/otc/urls.py
+++ otc/otc/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('app.urls')),
+    url(r'admin/', admin.site.urls),
+    url(r'app/', include('app.urls')),
     url(r'', include('login.urls')),
 ]
