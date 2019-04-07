5ddfc2c00a20261d71994a8d9df9bc771f7d42aa
johannakch <jojo.ko@icloud.com>
2019-04-07 12:35:10 +0200

    bla

--- otc/login/urls.py
+++ otc/login/urls.py
@@ -7,9 +7,9 @@ from .views import request_new_password
 urlpatterns = [
     url('login/', views.login, name='login'),
     url('logout/', views.logout, name='logout'),
-    url('^password-change/done/$', views.password_change_done,
+    url('password-change/done/', views.password_change_done,
         {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
-    url('^password-change/$', views.password_change,
+    url('password-change/', views.password_change,
         {'template_name': 'registration/password_change_form.html', 'post_change_redirect': 'password_change_done'}, name='password_change'),
-    url('^request-new-password/$', request_new_password, name='request_new_password'),
+    url('request-new-password/', request_new_password, name='request_new_password'),
 ]
